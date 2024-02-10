58b2b959644ae717461c7dfacb613fdbce326fd5
kirikq <144107450+kirikq@users.noreply.github.com>
2024-02-10 15:19:00 +0300

    Add files via upload

---
+++ Задача 1 .pas
@@ -0,0 +1,22 @@
+﻿program zad1;
+var sum,k,s,i:integer;A:array [1..20] of integer;
+begin
+  readln(s);
+  k:=0;
+  sum:=0;
+  write('A = ');
+  for i:=1 to 20 do 
+  begin
+  A[i]:=random(1,10);
+  write(A[i]:3);
+  end;
+  writeln();
+  for i:=1 to 20 do 
+  begin
+    sum += A[i];
+    k += 1;
+    if sum > s then
+      break
+  end;
+  writeln('k = ',k)
+end.
---
+++ Задача 2 .pas
@@ -0,0 +1,10 @@
+﻿program zad2;
+var str:string;len,k,i:integer;
+begin
+readln(str);
+len := length(str);
+for i:=1 to (len-1) do 
+  if (str[i] <> '') and (str[i+1] = ' ') or (i = len-1) then
+    k += 1;
+writeln(len,' ',k);
+end.
